511564b2fa780ed4f157cb51599c84078f167c22
Cova-ops <dacovasan00@gmail.com>
2023-04-25 20:08:39 -0600

    Add Script for deploy

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -5,8 +5,7 @@ sudo apt-get install python-software-properties -y
 sudo apt-add-repository ppa:chris-lea/node.js -y
 sudo apt-get update
 sudo apt-get install nodejs -y
-sudo apt-get install npm
-Y
+sudo apt-get install npm -y
 
 # Install pm2 module 
 # https://pm2.keymetrics.io/
