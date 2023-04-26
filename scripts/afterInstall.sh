076bf903ffa5f35e3bfc55c4acb7497dc71304ce
Cova-ops <dacovasan00@gmail.com>
2023-04-25 20:42:17 -0600

    Add Script for deploy

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -4,3 +4,4 @@ mkdir /home/ubuntu/back-server
 cd /home/ubuntu/back-server
 
 sudo npm install
+npm run build
--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -3,4 +3,4 @@
 # Stop all servers and start the server as a daemon
 pm2 ls
 pm2 delete back-server
-pm2 start /home/ubuntu/back-server/index.js --name "back-server"
+pm2 start /home/ubuntu/back-server/build/index.js --name "back-server"
