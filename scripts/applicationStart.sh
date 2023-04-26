01546fc0acc9f4a3c3eedc78691b287bddcd05e5
Cova-ops <dacovasan00@gmail.com>
2023-04-25 23:10:19 -0600

    Add Script for deploy

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -3,4 +3,7 @@
 # Stop all servers and start the server as a daemon
 pm2 ls
 pm2 delete back-server
+
+ls -la /home/ubuntu/back-server
+
 pm2 start /home/ubuntu/back-server/build/index.js --name "back-server"
